3e8dfc314873aa6fed29dec7f8da395eaabf70b7
m4444x <m4444x@users.sourceforge.net>
2006-06-18 07:29:34 +0000

    new spell: spEditStringOffset

---
+++ spells/stringpalette.cpp
@@ -0,0 +1,130 @@
+#include "../spellbook.h"
+
+#include <QDebug>
+#include <QDialog>
+#include <QLabel>
+#include <QLayout>
+#include <QLineEdit>
+#include <QListWidget>
+#include <QPushButton>
+
+class spEditStringOffset : public Spell
+{
+public:
+	QString name() const { return "Edit String Offset"; }
+	QString page() const { return ""; }
+	
+	bool isApplicable( const NifModel * nif, const QModelIndex & index )
+	{
+		return nif->getValue( index ).type() == NifValue::tStringOffset && getStringPalette( nif ).isValid();
+	}
+	
+	QModelIndex cast( NifModel * nif, const QModelIndex & index )
+	{
+		QModelIndex iPalette = getStringPalette( nif );
+		
+		QMap<QString, int> strings = readStringPalette( nif, iPalette );
+		QString string = getString( strings, nif->get<int>( index ) );
+		
+		QDialog dlg;
+		
+		QLabel * lb = new QLabel( & dlg );
+		lb->setText( "Select a string or enter a new one" );
+		
+		QListWidget * lw = new QListWidget( & dlg );
+		lw->addItems( strings.keys() );
+		
+		QLineEdit * le = new QLineEdit( & dlg );
+		le->setText( string );
+		
+		QObject::connect( lw, SIGNAL( currentTextChanged( const QString & ) ), le, SLOT( setText( const QString & ) ) );
+		QObject::connect( lw, SIGNAL( itemActivated( QListWidgetItem * ) ), & dlg, SLOT( accept() ) );
+		
+		QPushButton * bo = new QPushButton( "Ok", & dlg );
+		QObject::connect( bo, SIGNAL( clicked() ), & dlg, SLOT( accept() ) );
+		
+		QPushButton * bc = new QPushButton( "Cancel", & dlg );
+		QObject::connect( bc, SIGNAL( clicked() ), & dlg, SLOT( reject() ) );
+		
+		QGridLayout * grid = new QGridLayout;
+		dlg.setLayout( grid );
+		grid->addWidget( lb, 0, 0, 1, 2 );
+		grid->addWidget( lw, 1, 0, 1, 2 );
+		grid->addWidget( le, 2, 0, 1, 2 );
+		grid->addWidget( bo, 3, 0, 1, 1 );
+		grid->addWidget( bc, 3, 1, 1, 1 );
+		
+		if ( dlg.exec() != QDialog::Accepted )
+			return index;
+		
+		nif->set<int>( index, addString( nif, iPalette, le->text() ) );
+		
+		return index;
+	}
+	
+	static QModelIndex getStringPalette( const NifModel * nif )
+	{
+		QModelIndex iPalette;
+		for ( int n = 0; n < nif->getBlockCount(); n++ )
+		{
+			QModelIndex idx = nif->getBlock( n, "NiStringPalette" );
+			if ( idx.isValid() )
+			{
+				if ( ! iPalette.isValid() )
+					iPalette = idx;
+				else
+					return QModelIndex();
+			}
+		}
+		return iPalette;
+	}
+	
+	static QMap<QString,int> readStringPalette( const NifModel * nif, const QModelIndex & iPalette )
+	{
+		QByteArray bytes = nif->get<QByteArray>( iPalette, "Palette" );
+		QMap<QString,int> strings;
+		int x = 0;
+		while ( x < bytes.count() )
+		{
+			QString s( & bytes.data()[x] );
+			strings.insert( s, x );
+			x += s.length() + 1;
+		}
+		return strings;
+	}
+	
+	static QString getString( const QMap<QString,int> strings, int ofs )
+	{
+		if ( ofs >= 0 )
+		{
+			QMapIterator<QString,int> it( strings );
+			while ( it.hasNext() )
+			{
+				it.next();
+				if ( ofs == it.value() )
+					return it.key();
+			}
+		}
+		return QString();
+	}
+	
+	static int addString( NifModel * nif, const QModelIndex & iPalette, const QString & string )
+	{
+		if ( string.isEmpty() )
+			return 0xffffffff;
+		
+		QMap<QString, int> strings = readStringPalette( nif, iPalette );
+		if ( strings.contains( string ) )
+			return strings[ string ];
+		
+		QByteArray bytes = nif->get<QByteArray>( iPalette, "Palette" );
+		int ofs = bytes.count();
+		bytes += string.toAscii();
+		bytes.append( '\0' );
+		nif->set<QByteArray>( iPalette, "Palette", bytes );
+		return ofs;
+	}
+};
+
+REGISTER_SPELL( spEditStringOffset )
+
