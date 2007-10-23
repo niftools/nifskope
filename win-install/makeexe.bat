10767d750295948ddb37c9c09bc5a0f03c116689
Amorilia <amorilia@users.sourceforge.net>
2007-10-23 09:10:58 +0000

    batch file for preparing installer with single click (assumes Python is installed in default location)

---
+++ win-install/makeexe.bat
@@ -0,0 +1,17 @@
+set NAME=nifskope
+set VERSION=1.0.2
+
+del %NAME%-%VERSION%-windows.exe
+
+cd ..\..\docsys
+del doc\*.html
+dir doc\
+\Python25\python nifxml_doc.py
+
+pause
+
+cd ..\nifskope\win-install
+
+"%PROGRAMFILES%\NSIS\makensis.exe" /v3 %NAME%.nsi
+
+pause
