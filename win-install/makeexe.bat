e1a64adeec03006c3d979d81ce57da01236e48ff
Amorilia <amorilia@users.sourceforge.net>
2008-02-22 08:26:49 +0000

    make build script work for 64-bit systems

--- win-install/makeexe.bat
+++ win-install/makeexe.bat
@@ -14,6 +14,7 @@ pause
 
 cd ..\nifskope\win-install
 
-"%PROGRAMFILES%\NSIS\makensis.exe" /v3 %NAME%-msvc.nsi
+if exist "%PROGRAMFILES%\NSIS\makensis.exe" "%PROGRAMFILES%\NSIS\makensis.exe" /v3 %NAME%-mingw-dynamic.nsi
+if exist "%PROGRAMFILES(x86)%\NSIS\makensis.exe" "%PROGRAMFILES(x86)%\NSIS\makensis.exe" /v3 %NAME%-mingw-dynamic.nsi
 
-pause
+ pause
