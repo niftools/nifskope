57759a7e5e6559f562daf1d1be1aa8966eba01aa
Tazpn <tazpn@users.sourceforge.net>
2008-12-20 22:54:52 +0000

    update makeexe.bat to add current revsion to exe name

--- win-install/makeexe.bat
+++ win-install/makeexe.bat
@@ -1,16 +1,26 @@
 rem Quick build script to create the installer for release.
-
+@echo on
+setlocal
 set NAME=nifskope
 set VERSION=1.0.17
 
-del %NAME%-%VERSION%-windows.exe
+for %%i in (svnversion.exe) do IF EXIST "%%~$PATH:i" set SVNVERSION=%%~$PATH:i)
+IF NOT EXIST "%SVNVERSION%" set SVNVERSION=%SystemDrive%\svn\bin\svnversion.exe
+IF NOT EXIST "%SVNVERSION%" set SVNVERSION=%PROGRAMFILES%\TortoiseSVN\bin\svnversion.exe
+IF NOT EXIST "%SVNVERSION%" set SVNVERSION=%PROGRAMFILES%\Subversion\bin\svnversion.exe
+IF EXIST "%SVNVERSION%" for /f "delims=| usebackq" %%j in (`"%SVNVERSION%" ..`) do set VERSION=%VERSION%.%%j 
+
+del %NAME%-%VERSION%-windows.exe > nul
 
 cd ..\..\docsys
 del doc\*.html
-dir doc\
 \Python25\python nifxml_doc.py
 
-pause
+if EXIST "%QTDIR%\bin\lrelease.exe" (
+    pushd ..\nifskope\lang
+    for %%i in (*.ts) do call "%QTDIR%\bin\lrelease.exe" %%i
+    popd
+)
 
 cd ..\nifskope\win-install
 
@@ -18,3 +28,5 @@ if exist "%PROGRAMFILES%\NSIS\makensis.exe" "%PROGRAMFILES%\NSIS\makensis.exe" /
 if exist "%PROGRAMFILES(x86)%\NSIS\makensis.exe" "%PROGRAMFILES(x86)%\NSIS\makensis.exe" /v3 %NAME%-mingw-dynamic.nsi
 
 pause
+:TheEnd
+endlocal
