b3b38905a08200cc5b8299bd7724a37e5b47b91d
Alphax <alphasigmax@users.sourceforge.net>
2009-01-26 15:38:18 +0000

    sanity checking in makeconfig.sh

--- makeconfig.sh
+++ makeconfig.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 wcrev=`svn info | sed -n '/^Last Changed Rev/ s/[^0-9]//gp'`
-grep ${wcrev} config.h > /dev/null
-if [[ $? == 0 ]]; then
-    exit
-else
-    sed 's/\$WCREV\$/'${wcrev}'/' config.h.in > config.h
+if [[ -e config.h ]]; then
+    grep ${wcrev} config.h > /dev/null
+    if [[ $? == 0 ]]; then
+        exit
+    fi
 fi
+sed 's/\$WCREV\$/'${wcrev}'/' config.h.in > config.h
 
