c1e3750075c8c353a869bfbca2b3edbb0233c957
Amorilia <amorilia@users.sourceforge.net>
2011-03-12 12:37:51 +0000

    Separate nsi file for fedora cross compile.

--- win-install/makeexe.sh
+++ win-install/makeexe.sh
@@ -1,7 +1,7 @@
 NAME=nifskope
 VERSION=1.1.0.`git log -1 --pretty=format:%h`
 
-rm ${NAME}-${VERSION}-windows.exe
+rm nifskope-*.exe
 echo !define VERSION \"${VERSION}\" > nifversion.nsh
 echo !define BUILD_RELEASE_FOLDER \"../release\" >> nifversion.nsh
 echo !define DLL_RELEASE_FOLDER \"/usr/i686-pc-mingw32/sys-root/mingw/bin\" >> nifversion.nsh
@@ -18,4 +18,4 @@ cp ../qhull/COPYING.txt ../Qhull_COPYING.TXT
 
 cd ../win-install
 
-makensis -V3 ${NAME}-mingw-dynamic.nsi
+makensis -V3 ${NAME}-fedora-mingw-dynamic.nsi
