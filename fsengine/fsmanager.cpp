niftools/nifskope

commit fae06749d624fc105ad8a04fd72e894839d95e73

fix: another attempt to fix the block number visibility ( thats why you should not use text intended for rendering for anything else, but rendering )

fsengine/fsmanager.cpp
@@ -44,6 +44,8 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <QSettings>
 #include <QStringListModel>
 
+#include "../options.h"
+
 //! Global BSA file manager
 static FSManager *theFSManager = NULL;
 // see fsmanager.h
@@ -219,7 +221,7 @@ void FSSelector::sltAdd()
 
 void FSSelector::sltDel()
 {
-	QString an = view->currentIndex().data( Qt::DisplayRole ).toString();
+	QString an = view->currentIndex().data( NifSkopeDisplayRole ).toString();
 	
 	if ( FSArchiveHandler * a = manager->archives.take( an ) )
 	{

gl/glcontroller.cpp
@@ -32,7 +32,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "glcontroller.h"
 #include "glscene.h"
-
+#include "../options.h"
 /*
  *  Controllable
  */
@@ -167,7 +167,7 @@ Controller::Controller( const QModelIndex & index ) : iBlock( index )
 QString Controller::typeId() const
 {
 	if ( iBlock.isValid() )
-		return iBlock.data( Qt::DisplayRole ).toString();
+		return iBlock.data( NifSkopeDisplayRole ).toString();
 	return QString();
 }
 

gl/glmesh.cpp
@@ -942,12 +942,12 @@ void Mesh::drawSelection() const
 	}
 	else if ( scene->currentBlock == iData || scene->currentBlock == iSkinPart )
 	{
-		n = scene->currentIndex.data( Qt::DisplayRole ).toString();
+		n = scene->currentIndex.data( NifSkopeDisplayRole ).toString();
 		
 		QModelIndex iParent = scene->currentIndex.parent();
 		if ( iParent.isValid() && iParent != iData )
 		{
-			n = iParent.data( Qt::DisplayRole ).toString();
+			n = iParent.data( NifSkopeDisplayRole ).toString();
 			i = scene->currentIndex.row();
 		}
 	}

gl/glnode.cpp
@@ -276,27 +276,27 @@ public:
 						QString nodename = nif->get<QString>( iCB, "Node Name" );
 						if ( nodename.isEmpty() ) {
 							QModelIndex idx = nif->getIndex( iCB, "Node Name Offset" );
-							nodename = idx.sibling( idx.row(), NifModel::ValueCol ).data( Qt::DisplayRole ).toString();
+							nodename = idx.sibling( idx.row(), NifModel::ValueCol ).data( NifSkopeDisplayRole ).toString();
 						}
 						QString proptype = nif->get<QString>( iCB, "Property Type" );
 						if ( proptype.isEmpty() ) {
 							QModelIndex idx = nif->getIndex( iCB, "Property Type Offset" );
-							proptype = idx.sibling( idx.row(), NifModel::ValueCol ).data( Qt::DisplayRole ).toString();
+							proptype = idx.sibling( idx.row(), NifModel::ValueCol ).data( NifSkopeDisplayRole ).toString();
 						}
 						QString ctrltype = nif->get<QString>( iCB, "Controller Type" );
 						if ( ctrltype.isEmpty() ) {
 							QModelIndex idx = nif->getIndex( iCB, "Controller Type Offset" );
-							ctrltype = idx.sibling( idx.row(), NifModel::ValueCol ).data( Qt::DisplayRole ).toString();
+							ctrltype = idx.sibling( idx.row(), NifModel::ValueCol ).data( NifSkopeDisplayRole ).toString();
 						}
 						QString var1 = nif->get<QString>( iCB, "Variable 1" );
 						if ( var1.isEmpty() ) {
 							QModelIndex idx = nif->getIndex( iCB, "Variable 1 Offset" );
-							var1 = idx.sibling( idx.row(), NifModel::ValueCol ).data( Qt::DisplayRole ).toString();
+							var1 = idx.sibling( idx.row(), NifModel::ValueCol ).data( NifSkopeDisplayRole ).toString();
 						}
 						QString var2 = nif->get<QString>( iCB, "Variable 2" );
 						if ( var2.isEmpty() ) {
 							QModelIndex idx = nif->getIndex( iCB, "Variable 2 Offset" );
-							var2 = idx.sibling( idx.row(), NifModel::ValueCol ).data( Qt::DisplayRole ).toString();
+							var2 = idx.sibling( idx.row(), NifModel::ValueCol ).data( NifSkopeDisplayRole ).toString();
 						}
 						Node * node = target->findChild( nodename );
 						if ( ! node )
@@ -1101,11 +1101,11 @@ void drawHvkShape( const NifModel * nif, const QModelIndex & iShape, QStack<QMod
 			if (scene->currentBlock == iData )
 			{
 				int i = -1;
-				QString n = scene->currentIndex.data( Qt::DisplayRole ).toString();
+				QString n = scene->currentIndex.data( NifSkopeDisplayRole ).toString();
 				QModelIndex iParent = scene->currentIndex.parent();
 				if ( iParent.isValid() && iParent != iData )
 				{
-					n = iParent.data( Qt::DisplayRole ).toString();
+					n = iParent.data( NifSkopeDisplayRole ).toString();
 					i = scene->currentIndex.row();
 				}
 				if ( n == "Vertices" || n == "Normals" || n == "Vertex Colors" || n == "UV Sets" )
@@ -1143,11 +1143,11 @@ void drawHvkShape( const NifModel * nif, const QModelIndex & iShape, QStack<QMod
 			else if ( scene->currentBlock == iShape )
 			{
 				int i = -1;
-				QString n = scene->currentIndex.data( Qt::DisplayRole ).toString();
+				QString n = scene->currentIndex.data( NifSkopeDisplayRole ).toString();
 				QModelIndex iParent = scene->currentIndex.parent();
 				if ( iParent.isValid() && iParent != iShape )
 				{
-					n = iParent.data( Qt::DisplayRole ).toString();
+					n = iParent.data( NifSkopeDisplayRole ).toString();
 					i = scene->currentIndex.row();
 				}
 				//qDebug() << n;

nifdelegate.cpp
@@ -125,7 +125,11 @@ public:
 	
 	virtual void paint( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const
 	{
-		QString text = index.data( NifSkopeBlockNameRole ).toString();
+		int namerole = NifSkopeDisplayRole;
+		namerole = (index.isValid() && index.column() == 0) ?
+ 			Qt::DisplayRole : NifSkopeDisplayRole;
+		
+		QString text = index.data( namerole ).toString();
 		QString deco = index.data( Qt::DecorationRole ).toString();
 		
 		QString user = index.data( Qt::UserRole ).toString();
@@ -176,7 +180,7 @@ public:
 
 	QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
 	{
-		QString text = index.data( Qt::DisplayRole ).toString();
+		QString text = index.data( NifSkopeDisplayRole ).toString();
 		QRect textRect( 0, 0, option.fontMetrics.width(text), option.fontMetrics.lineSpacing() * (text.count(QLatin1Char('\n')) + 1) );
 		return textRect.size();
 	}
@@ -194,7 +198,7 @@ public:
 			NifValue nv = v.value<NifValue>();
 			if ( nv.isCount() && index.column() == NifModel::ValueCol )
 			{
-				NifValue::EnumType type = NifValue::enumType( index.sibling( index.row(), NifModel::TypeCol ).data( Qt::DisplayRole ).toString() );
+				NifValue::EnumType type = NifValue::enumType( index.sibling( index.row(), NifModel::TypeCol ).data( NifSkopeDisplayRole ).toString() );
 				if ( type == NifValue::eFlags) {
 					w = new NifCheckBoxList( parent );
 				} else if ( type == NifValue::eDefault) {
@@ -230,7 +234,7 @@ public:
 		else if ( cedit && v.canConvert<NifValue>() && v.value<NifValue>().isCount() )
 		{
 			cedit->clear();
-			QString t = index.sibling( index.row(), NifModel::TypeCol ).data( Qt::DisplayRole ).toString();
+			QString t = index.sibling( index.row(), NifModel::TypeCol ).data( NifSkopeDisplayRole ).toString();
 			const NifValue::EnumOptions& eo = NifValue::enumOptionData( t );
 			quint32 value = v.value<NifValue>().toCount();
 			QHashIterator< quint32, QPair< QString, QString > > it( eo.o );
@@ -267,7 +271,7 @@ public:
 		}
 		else if ( cedit )
 		{
-			QString t = index.sibling( index.row(), NifModel::TypeCol ).data( Qt::DisplayRole ).toString();
+			QString t = index.sibling( index.row(), NifModel::TypeCol ).data( NifSkopeDisplayRole ).toString();
 			QVariant v = index.data( Qt::EditRole );
 			bool ok;
 			quint32 x = NifValue::enumOptionValue( t, cedit->currentText(), &ok );

nifmodel.cpp
@@ -1057,30 +1057,23 @@ QVariant NifModel::data( const QModelIndex & idx, int role ) const
 		}
 	}
 
-
+	bool ndr = role == NifSkopeDisplayRole;
+	if (role == NifSkopeDisplayRole)
+		role = Qt::DisplayRole;
 	switch ( role )
 	{
-		case NifSkopeBlockNameRole:
-		{
-			switch ( column )
-			{
-				case NameCol:
-				{
-					QString a = "";
-					if ( itemType( index ) == "NiBlock" )
-						a = QString::number( getBlockNumber( index ) ) + " ";
-					return a + item->name();
-				}	break;
-				default: return QVariant();
-			}
-		}	break;
 		case Qt::DisplayRole:
 		{
 			switch ( column )
 			{
 				case NameCol:
 				{
-					return item->name();
+					if (ndr)
+						return item->name();
+					QString a = "";
+					if ( itemType( index ) == "NiBlock" )
+						a = QString::number( getBlockNumber( index ) ) + " ";
+					return a + item->name();
 				}	break;
 				case TypeCol:
 				{

options.h
@@ -36,7 +36,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <QObject>
 #include "widgets/groupbox.h"
 
-#define NifSkopeBlockNameRole (Qt::UserRole + 42)
+#define NifSkopeDisplayRole (Qt::UserRole + 42)
 
 //! \file options.h Options class
 

spells/animation.cpp
@@ -3,6 +3,8 @@
 #include <QDebug>
 #include <QFileDialog>
 
+#include "../options.h"
+
 // Brief description is deliberately not autolinked to class Spell
 /*! \file animation.cpp
  * \brief Animation editing spells
@@ -90,7 +92,7 @@ public:
 						QString nodeName = kf.string( iCtrlBlcks.child( r, 0 ), "Node Name", false );
 						if (nodeName.isEmpty()) {
 							QModelIndex iNodeName = kf.getIndex( iCtrlBlcks.child( r, 0 ), "Node Name Offset" );
-							nodeName = iNodeName.sibling( iNodeName.row(), NifModel::ValueCol ).data( Qt::DisplayRole ).toString();
+							nodeName = iNodeName.sibling( iNodeName.row(), NifModel::ValueCol ).data( NifSkopeDisplayRole ).toString();
 						}
 						QModelIndex iCtrlNode = findChildNode( nif, iRoot, nodeName );
 						if ( iCtrlNode.isValid() )

commit a099e2a7c58891ee56035a9ef0b00b5497223cb3

Fixed another regression introduced by fae06749d624fc105ad8a04fd72e894839d95e73 10x to 83ff0de9ecc56ab48fd5be1feeb7556558e2a4fb from "master"

fsengine/fsmanager.cpp
@@ -221,7 +221,7 @@ void FSSelector::sltAdd()
 
 void FSSelector::sltDel()
 {
-	QString an = view->currentIndex().data( NifSkopeDisplayRole ).toString();
+	QString an = view->currentIndex().data( Qt::DisplayRole ).toString();
 	
 	if ( FSArchiveHandler * a = manager->archives.take( an ) )
 	{