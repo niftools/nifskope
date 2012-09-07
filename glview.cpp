609fe42a960fe96f35a1fff2d0032fc938561207
Amorilia <amorilia@users.sourceforge.net>
2012-09-07 13:01:01 +0100

    Revert "Disable gluErrorString as it is not in GLee or QtOpenGL."
    This reverts commit 7f12c0ffe594ba26a4601fc56fc1ab388799404d.

--- glview.cpp
+++ glview.cpp
@@ -335,10 +335,10 @@ void GLView::initializeGL()
 		updateShaders();
 
 	// check for errors
-	// XXX disabled: gluErrorString not in GLee or QtOpenGL
-	//GLenum err;
-	//while ( ( err = glGetError() ) != GL_NO_ERROR )
-	//	qDebug() << tr("GL ERROR (init) : ") << (const char *) gluErrorString( err );
+	
+	GLenum err;
+	while ( ( err = glGetError() ) != GL_NO_ERROR )
+		qDebug() << tr("GL ERROR (init) : ") << (const char *) gluErrorString( err );
 }
 
 void GLView::glProjection( int x, int y )
@@ -557,10 +557,10 @@ void GLView::paintGL()
 	glPopMatrix();
 	
 	// check for errors
-	// XXX disabled: gluErrorString not in GLee or QtOpenGL
-	//GLenum err;
-	//while ( ( err = glGetError() ) != GL_NO_ERROR )
-	//	qDebug() << tr("GL ERROR (paint) : ") << (const char *) gluErrorString( err );
+	
+	GLenum err;
+	while ( ( err = glGetError() ) != GL_NO_ERROR )
+		qDebug() << tr("GL ERROR (paint): ") << (const char *) gluErrorString( err );
 	
 	// update fps counter
 	if ( fpsacc > 1.0 && fpscnt )
