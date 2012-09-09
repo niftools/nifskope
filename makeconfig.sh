f57ff99da8c2e7e12bf7e12cb5dd9355ca891818
Amorilia <amorilia@users.sourceforge.net>
2012-09-09 16:45:45 +0100

    Fix and simplify makeconfig.sh script.

--- makeconfig.sh
+++ makeconfig.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 version=`cat VERSION`
-
 wcrev=`git log -1 --pretty=format:%h`
-if [[ -e config.h ]]; then
-    grep ${wcrev} config.h > /dev/null
-    if [[ $? == 0 ]]; then
-        exit
-    fi
-fi
-sed 's/\$WCREV\$/'${wcrev}'/' config.h.in > config.h
-sed 's/\$VERSION\$/'${wcrev}'/' config.h.in > config.h
+cat config.h.in | sed 's/\$WCREV\$/'${wcrev}'/' | sed 's/\$VERSION\$/'${version}'/' > config.h
 
