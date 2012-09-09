f0a6d0c5ea4cd35b74c2a0d64357290cf9fc468e
Amorilia <amorilia@users.sourceforge.net>
2012-09-09 16:37:43 +0100

    Store version in a file, so we don't need to go and duplicate it everywhere (using follows http://www.python.org/dev/peps/pep-0386/#the-new-versioning-algorithm for formatting).

--- makeconfig.sh
+++ makeconfig.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+version=`cat VERSION`
+
 wcrev=`git log -1 --pretty=format:%h`
 if [[ -e config.h ]]; then
     grep ${wcrev} config.h > /dev/null
@@ -8,4 +10,5 @@ if [[ -e config.h ]]; then
     fi
 fi
 sed 's/\$WCREV\$/'${wcrev}'/' config.h.in > config.h
+sed 's/\$VERSION\$/'${wcrev}'/' config.h.in > config.h
 
