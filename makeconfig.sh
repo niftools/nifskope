8b08e68da7586ebb3c9ac6c399a4a2804513b7f3
Amorilia <amorilia@users.sourceforge.net>
2012-09-09 16:51:49 +0100

    Use cmake style @OPTION@, also use VERSION file for Doxygen.

--- makeconfig.sh
+++ makeconfig.sh
@@ -2,5 +2,6 @@
 
 version=`cat VERSION`
 wcrev=`git log -1 --pretty=format:%h`
-cat config.h.in | sed 's/\$WCREV\$/'${wcrev}'/' | sed 's/\$VERSION\$/'${version}'/' > config.h
+cat config.h.in | sed 's/@WCREV@/'${wcrev}'/' | sed 's/@VERSION@/'${version}'/' > config.h
+cat Doxyfile.in | sed 's/@WCREV@/'${wcrev}'/' | sed 's/@VERSION@/'${version}'/' > Doxyfile
 
