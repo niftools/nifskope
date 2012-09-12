40bd43ae3e46c21a47cbb2d5a2a2b1cd8ec6d2f4
Amorilia <amorilia@users.sourceforge.net>
2012-09-12 13:32:44 +0100

    Remove unused @WCREV@ replacements.

--- makeconfig.sh
+++ makeconfig.sh
@@ -2,6 +2,6 @@
 
 version=`cat VERSION`
 wcrev=`git log -1 --pretty=format:%h`
-cat Doxyfile.in | sed 's/@WCREV@/'${wcrev}'/' | sed 's/@VERSION@/'${version}'/' > Doxyfile
-cat README.txt.in | sed 's/@WCREV@/'${wcrev}'/' | sed 's/@VERSION@/'${version}'/' > README.txt
-cat linux-install/nifskope.spec.in | sed 's/@WCREV@/'${wcrev}'/' | sed 's/@VERSION@/'${version}'/' > linux-install/nifskope.spec
+cat Doxyfile.in | sed 's/@VERSION@/'${version}'/' > Doxyfile
+cat README.txt.in | sed 's/@VERSION@/'${version}'/' > README.txt
+cat linux-install/nifskope.spec.in | sed 's/@VERSION@/'${version}'/' > linux-install/nifskope.spec
