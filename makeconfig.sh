b36fff3886a8a798c209e6a943f36e3ce6a1d23c
Amorilia <amorilia@users.sourceforge.net>
2012-09-12 13:33:34 +0100

    Remove wcrev code from makeconfig.sh (it also now no longer needs git).

--- makeconfig.sh
+++ makeconfig.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 version=`cat VERSION`
-wcrev=`git log -1 --pretty=format:%h`
 cat Doxyfile.in | sed 's/@VERSION@/'${version}'/' > Doxyfile
 cat README.txt.in | sed 's/@VERSION@/'${version}'/' > README.txt
 cat linux-install/nifskope.spec.in | sed 's/@VERSION@/'${version}'/' > linux-install/nifskope.spec
