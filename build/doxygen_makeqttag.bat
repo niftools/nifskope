dc7bb224cdb8c72d2c565f0f44381eb3f4aa6102
jonwd7 <jon.wd7@gmail.com>
2014-03-08 01:41:34 -0500

    [Restructure] Remove outdated doxygen .sh/.bat
    doxytag is obsolete and doesn't work on the Qt5 docs anyway (I tried).
    For the actual doxygen .sh/.bat, the Makefile generated by qmake has a
    build target for running doxygen:  `jom release-docs` or `make
    release-docs` (or nmake/mingw32-make)

--- build/doxygen_makeqttag.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\doxygen\bin\doxytag.exe" -t qt.tag C:\Qt\4.5.0\doc\html
--- build/doxygen_makeqttag.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# note: on Fedora, you need the qt-doc package for this to work
-doxytag -t qt.tag /usr/share/doc/qt4/html/
-
