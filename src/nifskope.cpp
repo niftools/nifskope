db256ae20c9409df11de58515d7dc2be1857fef1
jonwd7 <jon.wd7@gmail.com>
2014-05-17 19:10:31 -0400

    [BSLODTriShape] Fix warning on close

--- src/nifskope.cpp
+++ src/nifskope.cpp
@@ -408,6 +408,8 @@ NifSkope::NifSkope()
 
 	// LOD Toolbar
 	QToolBar * tLOD = new QToolBar( "LOD" );
+	tLOD->setObjectName( tr( "tLOD" ) );
+	tLOD->setAllowedAreas( Qt::TopToolBarArea | Qt::BottomToolBarArea );
 
 	NIFSKOPE_QSETTINGS( cfg );
 	cfg.beginGroup( "LOD" );
