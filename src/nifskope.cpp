niftools/nifskope

commit 8ed142819d2fd1b446ce7a2229c4905c33b8fbfc

[NifSkopeVersion][Migration] Massive QSettings cleanup
#61, #11

A `config.h` file was unnecessary as the default QSettings constructor
takes into account the values set in `setOrganizationName` and
`setApplicationName` in the main() function.

Thus, removed the use of the macro.

Reorganized multiple settings. Renamed, fixed typos, consistent
capitalization.

src/nifskope.cpp
@@ -31,7 +31,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***** END LICENCE BLOCK *****/
 
 #include "nifskope.h"
-#include "config.h"
+#include "version.h"
 #include "options.h"
 
 #include "glview.h"
@@ -89,25 +89,105 @@ FSManager * fsmanager = nullptr;
 
 void NifSkope::migrateSettings() const
 {
-	// load current NifSkope settings
-	NIFSKOPE_QSETTINGS( cfg );
+	// IMPORTANT:
+	//	Do not make any calls to Options:: until after all migration code.
+	//	Static calls to Options:: still create the options instance and inits
+	//	the various widgets with incorrect values. Once you close the app, 
+	//	the settings you migrated get overwritten with the default values.
 
-	// check if we are running a new version of NifSkope
-	if ( cfg.value( "version" ).toString() != NIFSKOPE_VERSION ) {
-		// check all keys and delete all binary ones
-		//to prevent portability problems between Qt versions
+	// Load current NifSkope settings
+	QSettings cfg;
+	// Load pre-1.2 NifSkope settings
+	QSettings cfg1_1( "NifTools", "NifSkope" );
+
+	// Current version strings
+	QString curVer = NIFSKOPE_VERSION;
+	QString curQtVer = QT_VERSION_STR;
+	QString curDisplayVer = NifSkopeVersion::rawToDisplay( NIFSKOPE_VERSION, true );
+
+	bool doMigration = false;
+
+	// New Install, no need to migrate anything
+	if ( !cfg.value( "Version" ).isValid() && !cfg1_1.value( "version" ).isValid() ) {
+		// QSettings constructor creates an empty folder, so clear it. 
+		cfg1_1.clear();
+
+		// Set version values
+		cfg.setValue( "Version", curVer );
+		cfg.setValue( "Qt Version", curQtVer );
+		cfg.setValue( "Display Version", curDisplayVer );
+
+		return;
+	}
+
+	// Forward dec for prevVer which either comes from `cfg` or `cfg1_1`
+	QString prevVer;
+	QString prevQtVer = cfg.value( "Qt Version" ).toString();
+	QString prevDisplayVer = cfg.value( "Display Version" ).toString();
+
+	// Set full granularity for version comparisons
+	NifSkopeVersion::setNumParts( 7 );
+
+	// Check for Existing 1.1 Migration
+	if ( !cfg1_1.value( "migrated" ).isValid() ) {
+		// Old install has not been migrated yet
+
+		// Get prevVer from pre-1.2 settings
+		prevVer = cfg1_1.value( "version" ).toString();
+
+		NifSkopeVersion tmp( prevVer );
+		if ( tmp < "1.2.0" )
+			doMigration = true;
+	} else {
+		// Get prevVer from post-1.2 settings
+		prevVer = cfg.value( "Version" ).toString();
+	}
+
+	NifSkopeVersion oldVersion( prevVer );
+	NifSkopeVersion newVersion( curVer );
+
+	// Migrate from 1.1.x to 1.2
+	if ( doMigration && (oldVersion < "1.2.0") ) {
+		// Port old key values to new key names
+		QHash<QString, QString>::const_iterator i;
+		for ( i = migrateTo1_2.begin(); i != migrateTo1_2.end(); ++i ) {
+			QVariant val = cfg1_1.value( i.key() );
+
+			if ( val.isValid() ) {
+				cfg.setValue( i.value(), val );
+			}
+		}
+
+		// Set `migrated` flag in legacy QSettings
+		cfg1_1.setValue( "migrated", true );
+	}
+
+	// Check NifSkope Version
+	//	Assure full granularity here
+	NifSkopeVersion::setNumParts( 7 );
+	if ( oldVersion != newVersion ) {
+		// Set new Version
+		cfg.setValue( "Version", curVer );
+
+		if ( prevDisplayVer != curDisplayVer )
+			cfg.setValue( "Display Version", curDisplayVer );
+	}
+
+	// Check Qt Version
+	if ( curQtVer != prevQtVer ) {
+		// Check all keys and delete all QByteArrays
+		// to prevent portability problems between Qt versions
 		QStringList keys = cfg.allKeys();
 
 		for ( const auto& key : keys ) {
 			if ( cfg.value( key ).type() == QVariant::ByteArray ) {
-				qDebug() << "removing config setting" << key
-				         << "whilst migrating settings from previous NifSkope version";
+				qDebug() << "Removing Qt version-specific settings" << key
+					<< "while migrating settings from previous version";
 				cfg.remove( key );
 			}
 		}
 
-		// set version key to current version
-		cfg.setValue( "version", NIFSKOPE_VERSION );
+		cfg.setValue( "Qt Version", curQtVer );
 	}
 }
 
@@ -411,11 +491,9 @@ NifSkope::NifSkope()
 	tLOD->setObjectName( tr( "tLOD" ) );
 	tLOD->setAllowedAreas( Qt::TopToolBarArea | Qt::BottomToolBarArea );
 
-	NIFSKOPE_QSETTINGS( cfg );
-	cfg.beginGroup( "LOD" );
-	int lodLevel = cfg.value( "LOD Level", 2 ).toInt();
-	cfg.setValue( "LOD Level", lodLevel );
-	cfg.endGroup();
+	QSettings cfg;
+	int lodLevel = cfg.value( "GLView/LOD Level", 2 ).toInt();
+	cfg.setValue( "GLView/LOD Level", lodLevel );
 
 	QSlider * lodSlider = new QSlider( Qt::Horizontal );
 	lodSlider->setFocusPolicy( Qt::StrongFocus );
@@ -431,10 +509,8 @@ NifSkope::NifSkope()
 
 	connect( lodSlider, &QSlider::valueChanged, []( int value )
 		{
-			NIFSKOPE_QSETTINGS( cfg );
-			cfg.beginGroup( "LOD" );
-			cfg.setValue( "LOD Level", value );
-			cfg.endGroup();
+			QSettings cfg;
+			cfg.setValue( "GLView/LOD Level", value );
 		}
 	);
 	connect( lodSlider, &QSlider::valueChanged, Options::get(), &Options::sigChanged );
@@ -527,7 +603,7 @@ NifSkope::~NifSkope()
 
 void NifSkope::closeEvent( QCloseEvent * e )
 {
-	NIFSKOPE_QSETTINGS( settings );
+	QSettings settings;
 	save( settings );
 
 	QMainWindow::closeEvent( e );
@@ -556,29 +632,29 @@ void restoreHeader( const QString & name, const QSettings & settings, QHeaderVie
 
 void NifSkope::restore( const QSettings & settings )
 {
-	restoreGeometry( settings.value( "window geometry" ).toByteArray() );
-	restoreState( settings.value( "window state" ).toByteArray(), 0x073 );
+	restoreGeometry( settings.value( "UI/Window Geometry" ).toByteArray() );
+	restoreState( settings.value( "UI/Window State" ).toByteArray(), 0x073 );
 
-	lineLoad->setText( settings.value( "last load", QString( "" ) ).toString() );
-	lineSave->setText( settings.value( "last save", QString( "" ) ).toString() );
-	aSanitize->setChecked( settings.value( "auto sanitize", true ).toBool() );
+	lineLoad->setText( settings.value( "File/Last Load", QString( "" ) ).toString() );
+	lineSave->setText( settings.value( "File/Last Save", QString( "" ) ).toString() );
+	aSanitize->setChecked( settings.value( "File/Auto Sanitize", true ).toBool() );
 
-	if ( settings.value( "list mode", "hirarchy" ).toString() == "list" )
+	if ( settings.value( "UI/List Mode", "hierarchy" ).toString() == "list" )
 		aList->setChecked( true );
 	else
 		aHierarchy->setChecked( true );
 
 	setListMode();
 
-	aCondition->setChecked( settings.value( "hide condition zero", false ).toBool() );
-	aRCondition->setChecked( settings.value( "realtime condition updating", false ).toBool() );
-	restoreHeader( "list sizes", settings, list->header() );
-	restoreHeader( "tree sizes", settings, tree->header() );
-	restoreHeader( "kfmtree sizes", settings, kfmtree->header() );
+	aCondition->setChecked( settings.value( "UI/Hide Mismatched Rows", false ).toBool() );
+	aRCondition->setChecked( settings.value( "UI/Realtime Condition Updating", false ).toBool() );
+	restoreHeader( "UI/List Sizes", settings, list->header() );
+	restoreHeader( "UI/Tree Sizes", settings, tree->header() );
+	restoreHeader( "UI/Kfmtree Sizes", settings, kfmtree->header() );
 
 	ogl->restore( settings );
 
-	QVariant fontVar = settings.value( "viewFont" );
+	QVariant fontVar = settings.value( "UI/View Font" );
 
 	if ( fontVar.canConvert<QFont>() )
 		setViewFont( fontVar.value<QFont>() );
@@ -599,20 +675,20 @@ void saveHeader( const QString & name, QSettings & settings, QHeaderView * heade
 
 void NifSkope::save( QSettings & settings ) const
 {
-	settings.setValue( "window state", saveState( 0x073 ) );
-	settings.setValue( "window geometry", saveGeometry() );
+	settings.setValue( "UI/Window State", saveState( 0x073 ) );
+	settings.setValue( "UI/Window Geometry", saveGeometry() );
 
-	settings.setValue( "last load", lineLoad->text() );
-	settings.setValue( "last save", lineSave->text() );
-	settings.setValue( "auto sanitize", aSanitize->isChecked() );
+	settings.setValue( "File/Last Load", lineLoad->text() );
+	settings.setValue( "File/Last Save", lineSave->text() );
+	settings.setValue( "File/Auto Sanitize", aSanitize->isChecked() );
 
-	settings.setValue( "list mode", ( gListMode->checkedAction() == aList ? "list" : "hirarchy" ) );
-	settings.setValue( "hide condition zero", aCondition->isChecked() );
-	settings.setValue( "realtime condition updating", aRCondition->isChecked() );
+	settings.setValue( "UI/List Mode", ( gListMode->checkedAction() == aList ? "list" : "hierarchy" ) );
+	settings.setValue( "UI/Hide Mismatched Rows", aCondition->isChecked() );
+	settings.setValue( "UI/Realtime Condition Updating", aRCondition->isChecked() );
 
-	saveHeader( "list sizes", settings, list->header() );
-	saveHeader( "tree sizes", settings, tree->header() );
-	saveHeader( "kfmtree sizes", settings, kfmtree->header() );
+	saveHeader( "UI/List Sizes", settings, list->header() );
+	saveHeader( "UI/Tree Sizes", settings, tree->header() );
+	saveHeader( "UI/Kfmtree Sizes", settings, kfmtree->header() );
 
 	ogl->save( settings );
 
@@ -780,7 +856,6 @@ void NifSkope::load()
 	if ( !niffile.isFile() ) {
 		nif->clear();
 		lineLoad->setState( FileSelector::stError );
-		setWindowTitle( "NifSkope" );
 	} else {
 		ProgDlg prog;
 		prog.setLabelText( tr( "loading nif..." ) );
@@ -801,7 +876,7 @@ void NifSkope::load()
 			lineSave->setText( niffile.filePath() );
 		}
 
-		setWindowTitle( niffile.fileName() + " - NifSkope" );
+		setWindowTitle( niffile.fileName() );
 	}
 
 	ogl->tAnim->setEnabled( true );
@@ -859,7 +934,7 @@ void NifSkope::save()
 			lineSave->setState( FileSelector::stSuccess );
 		}
 
-		setWindowTitle( nifname.right( nifname.length() - nifname.lastIndexOf( '/' ) - 1 ) + " - NifSkope" );
+		setWindowTitle( nif->getFileInfo().fileName() );
 	}
 
 	setEnabled( true );
@@ -916,7 +991,7 @@ NifSkope * NifSkope::createWindow( const QString & fname )
 {
 	NifSkope * skope = new NifSkope;
 	skope->setAttribute( Qt::WA_DeleteOnClose );
-	NIFSKOPE_QSETTINGS( settings );
+	QSettings settings;
 	skope->restore( settings );
 	skope->show();
 
@@ -953,7 +1028,7 @@ void NifSkope::sltSelectFont()
 
 	setViewFont( fnt );
 	QSettings settings;
-	settings.setValue( "viewFont", fnt );
+	settings.setValue( "UI/View Font", fnt );
 }
 
 void NifSkope::setViewFont( const QFont & font )
@@ -980,7 +1055,7 @@ bool NifSkope::eventFilter( QObject * o, QEvent * e )
 void NifSkope::overrideViewFont()
 {
 	QSettings settings;
-	QVariant var = settings.value( "viewFont" );
+	QVariant var = settings.value( "UI/View Font" );
 
 	if ( var.canConvert<QFont>() ) {
 		setViewFont( var.value<QFont>() );
@@ -1252,8 +1327,10 @@ int main( int argc, char * argv[] )
 	// set up the Qt Application
 	QApplication app( argc, argv );
 	app.setOrganizationName( "NifTools" );
-	app.setApplicationName( "NifSkope" );
-	app.setOrganizationDomain( "niftools.sourceforge.net" );
+	app.setOrganizationDomain( "niftools.org" );
+	app.setApplicationName( "NifSkope " + NifSkopeVersion::rawToMajMin( NIFSKOPE_VERSION ) );
+	app.setApplicationVersion( NIFSKOPE_VERSION );
+	app.setApplicationDisplayName( "NifSkope " + NifSkopeVersion::rawToDisplay( NIFSKOPE_VERSION, true ) );
 
 	// install message handler
 	qRegisterMetaType<Message>( "Message" );
@@ -1287,7 +1364,7 @@ int main( int argc, char * argv[] )
 		}
 	}
 
-	NIFSKOPE_QSETTINGS( cfg );
+	QSettings cfg;
 	cfg.beginGroup( "Settings" );
 	SetAppLocale( cfg.value( "Language", "en" ).toLocale() );
 	cfg.endGroup();
@@ -1354,3 +1431,4 @@ int main( int argc, char * argv[] )
 
 	}
 }
+

lib/fsengine/fsmanager.cpp
@@ -70,10 +70,8 @@ FSManager::FSManager( QObject * parent )
 	: QObject( parent ), automatic( false )
 {
 	QSettings cfg;
-	cfg.beginGroup( "fsengine" );
-	
-	QStringList list = cfg.value( "archives", QStringList() ).toStringList();
-	
+	QStringList list = cfg.value( "FSEngine/Archives", QStringList() ).toStringList();
+
 	if ( list.size() == 1 && list.first() == "AUTO" )
 	{
 		automatic = true;
@@ -174,9 +172,8 @@ FSSelector::FSSelector( FSManager * m )
 FSSelector::~FSSelector()
 {
 	QSettings cfg;
-	cfg.beginGroup( "fsengine" );
 	QStringList list( manager->automatic ? QStringList() << "AUTO" : manager->archives.keys() );
-	cfg.setValue( "archives", list );
+	cfg.setValue( "FSEngine/Archives", list );
 
 	emit Options::get()->sigFlush3D();
 }

src/config.h
@@ -34,6 +34,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define CONFIG_H
 
 //#include <QStringList>
+//#include "version.h"
 
 
 /*! \file config.h
@@ -43,6 +44,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 //! Create or use a QSettings variable for NifSkope
-#define NIFSKOPE_QSETTINGS( config ) QSettings config( "NifTools", "NifSkope" )
+//#define NIFSKOPE_QSETTINGS( config ) QSettings config
+
 
 #endif

src/gl/glmesh.cpp
@@ -981,10 +981,8 @@ void Mesh::drawShapes( NodeList * draw2nd )
 		auto lod1tris = sortedTriangles.mid( lod0, lod1 );
 		auto lod2tris = sortedTriangles.mid( lod0 + lod1, lod2 );
 
-		NIFSKOPE_QSETTINGS( cfg );
-		cfg.beginGroup( "LOD" );
-		int lodLevel = cfg.value( "LOD Level", 2 ).toInt();
-		cfg.endGroup();
+		QSettings cfg;
+		int lodLevel = cfg.value( "GLView/LOD Level", 2 ).toInt();
 
 		// render level 0 (always visible)
 		if ( lod0tris.count() )

src/glview.cpp
@@ -1210,34 +1210,29 @@ void GLView::checkActions()
 
 void GLView::save( QSettings & settings )
 {
-	//settings.beginGroup( "OpenGL" );
-	settings.setValue( "enable animations", aAnimate->isChecked() );
-	settings.setValue( "play animation", aAnimPlay->isChecked() );
-	settings.setValue( "loop animation", aAnimLoop->isChecked() );
-	settings.setValue( "switch animation", aAnimSwitch->isChecked() );
-	settings.setValue( "perspective", aViewPerspective->isChecked() );
+	settings.setValue( "GLView/Enable Animations", aAnimate->isChecked() );
+	settings.setValue( "GLView/Play Animation", aAnimPlay->isChecked() );
+	settings.setValue( "GLView/Loop Animation", aAnimLoop->isChecked() );
+	settings.setValue( "GLView/Switch Animation", aAnimSwitch->isChecked() );
+	settings.setValue( "GLView/Perspective", aViewPerspective->isChecked() );
 
 	if ( checkedViewAction() )
-		settings.setValue( "view action", checkedViewAction()->text() );
-
-	//settings.endGroup();
+		settings.setValue( "GLView/View Action", checkedViewAction()->text() );
 }
 
 void GLView::restore( const QSettings & settings )
 {
-	//settings.beginGroup( "OpenGL" );
-	aAnimate->setChecked( settings.value( "enable animations", true ).toBool() );
-	aAnimPlay->setChecked( settings.value( "play animation", true ).toBool() );
-	aAnimLoop->setChecked( settings.value( "loop animation", true ).toBool() );
-	aAnimSwitch->setChecked( settings.value( "switch animation", true ).toBool() );
-	aViewPerspective->setChecked( settings.value( "perspective", true ).toBool() );
+	aAnimate->setChecked( settings.value( "GLView/Enable Animations", true ).toBool() );
+	aAnimPlay->setChecked( settings.value( "GLView/Play Animation", true ).toBool() );
+	aAnimLoop->setChecked( settings.value( "GLView/Loop Animation", true ).toBool() );
+	aAnimSwitch->setChecked( settings.value( "GLView/Switch Animation", true ).toBool() );
+	aViewPerspective->setChecked( settings.value( "GLView/Perspective", true ).toBool() );
 	checkActions();
-	QString viewAct = settings.value( "view action", "Front" ).toString();
+	QString viewAct = settings.value( "GLView/View Action", "Front" ).toString();
 	for ( QAction * act : grpView->actions() ) {
 		if ( act->text() == viewAct )
 			viewAction( act );
 	}
-	//settings.endGroup();
 }
 
 void GLView::saveImage()
@@ -1277,11 +1272,9 @@ void GLView::saveImage()
 	lay->addWidget( niffileDir, 1, 1, 1, 1 );
 
 	// Save JPEG Quality
-	NIFSKOPE_QSETTINGS( cfg );
-	cfg.beginGroup( "JPEG" );
-	int jpegQuality = cfg.value( "Quality", 91 ).toInt();
-	cfg.setValue( "Quality", jpegQuality );
-	cfg.endGroup();
+	QSettings cfg;
+	int jpegQuality = cfg.value( "JPEG/Quality", 91 ).toInt();
+	cfg.setValue( "JPEG/Quality", jpegQuality );
 
 	QHBoxLayout * pixBox = new QHBoxLayout;
 	pixBox->setAlignment( Qt::AlignRight );
@@ -1321,9 +1314,7 @@ void GLView::saveImage()
 	connect( btnOk, &QPushButton::clicked, [&]() 
 		{
 			// Save JPEG Quality
-			cfg.beginGroup( "JPEG" );
-			cfg.setValue( "Quality", pixQuality->value() );
-			cfg.endGroup();
+			cfg.setValue( "JPEG/Quality", pixQuality->value() );
 
 			// TODO: Set up creation of screenshots directory in Options
 			if ( nifskopeDir->isChecked() ) {

src/importex/3ds.cpp
@@ -240,8 +240,8 @@ void import3ds( NifModel * nif, const QModelIndex & index )
 	QMap<QString, objKfSequence> ObjKeyframes;
 
 	QSettings settings;
-	settings.beginGroup( "import-export" );
-	settings.beginGroup( "3ds" );
+	settings.beginGroup( "Import-Export" );
+	settings.beginGroup( "3DS" );
 
 	QString fname = QFileDialog::getOpenFileName( qApp->activeWindow(), tr( "Choose a .3ds file to import" ), settings.value( tr( "File Name" ) ).toString(), "3DS (*.3ds)" );
 
@@ -744,6 +744,9 @@ void import3ds( NifModel * nif, const QModelIndex & index )
 	}
 
 	settings.setValue( "File Name", fname );
+
+	settings.endGroup(); // 3DS
+	settings.endGroup(); // Import-Export
 
 	nif->reset();
 	return;

src/importex/col.cpp
@@ -969,8 +969,8 @@ void exportCol( const NifModel * nif, QFileInfo fileInfo )
 	QList<int> roots = nif->getRootLinks();
 	QString question;
 	QSettings settings;
-	settings.beginGroup( "import-export" );
-	settings.beginGroup( "col" );
+	settings.beginGroup( "Import-Export" );
+	settings.beginGroup( "COLLADA" );
 
 	QString fname = QFileDialog::getSaveFileName( qApp->activeWindow(), tr( "Choose a .DAE file for export" ), QString( "%1%2.dae" ).arg( settings.value( "Path" ).toString() ).arg( fileInfo.baseName() ), "COLLADA (*.dae)" );
 
@@ -1048,5 +1048,8 @@ void exportCol( const NifModel * nif, QFileInfo fileInfo )
 	settings.setValue( "Path", QString( "%1/" ).arg( QFileInfo( fobj.fileName() ).path() ) );
 	QTextStream sobj( &fobj ); // let's save xml
 	fobj.close();
+
+	settings.endGroup(); // COLLADA
+	settings.endGroup(); // Import-Export
 }
 

src/importex/obj.cpp
@@ -331,8 +331,8 @@ void exportObj( const NifModel * nif, const QModelIndex & index )
 	//--Allow the user to select the file--//
 
 	QSettings settings;
-	settings.beginGroup( "import-export" );
-	settings.beginGroup( "obj" );
+	settings.beginGroup( "Import-Export" );
+	settings.beginGroup( "OBJ" );
 
 	QString fname = QFileDialog::getSaveFileName( qApp->activeWindow(), tr( "Choose a .OBJ file for export" ), settings.value( "File Name" ).toString(), "OBJ (*.obj)" );
 
@@ -382,6 +382,9 @@ void exportObj( const NifModel * nif, const QModelIndex & index )
 	}
 
 	settings.setValue( "File Name", fobj.fileName() );
+
+	settings.endGroup(); // OBJ
+	settings.endGroup(); // Import-Export
 }
 
 
@@ -557,8 +560,8 @@ void importObj( NifModel * nif, const QModelIndex & index )
 	//--Read the file--//
 
 	QSettings settings;
-	settings.beginGroup( "import-export" );
-	settings.beginGroup( "obj" );
+	settings.beginGroup( "Import-Export" );
+	settings.beginGroup( "OBJ" );
 
 	QString fname = QFileDialog::getOpenFileName( qApp->activeWindow(), tr( "Choose a .OBJ file to import" ), settings.value( "File Name" ).toString(), "OBJ (*.obj)" );
 
@@ -992,6 +995,9 @@ void importObj( NifModel * nif, const QModelIndex & index )
 	qDeleteAll( ofaces );
 
 	settings.setValue( "File Name", fname );
+
+	settings.endGroup(); // OBJ
+	settings.endGroup(); // Import-Export
 
 	nif->reset();
 }

commit 4ad091fc9677ceaa462c5a8c0aca74aa02fb5b55

[Fix] Update window title when saving NIF
When the NIF filename is changed (i.e. saving to a new file) the window
title was no longer updating.  Caused in 8ed1428

src/nifskope.cpp
@@ -832,7 +832,11 @@ void NifSkope::save()
 			lineSave->setState( FileSelector::stSuccess );
 		}
 
-		setWindowTitle( nif->getFileInfo().fileName() );
+		// TODO: nif->getFileInfo() returns stale data
+		// Instead create tmp QFileInfo from lineSave text
+		// Future: updating file info stored in nif
+		QFileInfo finfo( nifname );
+		setWindowTitle( finfo.fileName() );
 	}
 
 	setEnabled( true );