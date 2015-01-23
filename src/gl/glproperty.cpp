026c2f7109db1b19c4dcb93f7cfc68357fc1c2e5
jonwd7 <jon.wd7@gmail.com>
2015-01-23 12:10:48 -0500

    [GL] Enable seamless wrapping for cube maps

--- src/gl/glproperty.cpp
+++ src/gl/glproperty.cpp
@@ -874,6 +874,8 @@ bool BSShaderLightingProperty::bindCube( int id, const QString & fname )
 	if ( result == 0 )
 		return false;
 
+	glEnable( GL_TEXTURE_CUBE_MAP_SEAMLESS );
+
 	glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
 	glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
 	glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
