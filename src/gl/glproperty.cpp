41e5bc7d7f06e1e39864f96865c56f9b10715ed5
jonwd7 <jon.wd7@gmail.com>
2015-01-23 11:48:18 -0500

    [GL] Fix cube map wrap params to reduce seams

--- src/gl/glproperty.cpp
+++ src/gl/glproperty.cpp
@@ -874,8 +874,8 @@ bool BSShaderLightingProperty::bindCube( int id, const QString & fname )
 	if ( result == 0 )
 		return false;
 
-	glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_REPEAT );
-	glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_REPEAT );
+	glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+	glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
 	glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
 	glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
 	glTexEnvi( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
