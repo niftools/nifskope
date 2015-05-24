3ea50405f88970ea94c59d81df2f3666617807f2
tschilkroete <paul.trojahn@gmx.de>
2015-05-24 12:41:41 +0200

    Revert "[Script] Copy, paste, delete and multiple file support"
    This reverts commit f42e11bf5bd06f0282191c0caa6b785c4308ad84.

--- src/nifscript.h
+++ src/nifscript.h
@@ -25,14 +25,12 @@ private:
     bool loadCommand(QString path);
     bool importCommand(int block, QString path);
     bool saveCommand(QString path);
-    bool spellCommand(QString spell, int block);
 
     NifModel *nif;
     QString scriptPath;
 
 public slots:
     void okButtonPressed();
-    void executeScript();
 };
 
 #endif // NIFSCRIPT_H
