niftools/nifskope

commit 31fc1b2a16a3a05960d2ff369b7b4f398060e855

[GL] Renderer refactor, stage 1
Changed nearly all of setupProgram for the shaders.  Store the uniform locations, use enums and maps instead of strings to refer to the uniforms.  This helps with speed, legibility, and autocompletion.

Removed all the `ob_` shaders for the time being as there is a shader complexity issue (more shaders == worse).  Will reinstate fo3 and ob shading over time and with far fewer shaders.

src/gl/glmesh.cpp
@@ -116,6 +116,9 @@ void Shape::updateShaderProperties( const NifModel * nif )
 	QModelIndex iLSP = nif->getBlock( prop, "BSLightingShaderProperty" );
 	QModelIndex iESP = nif->getBlock( prop, "BSEffectShaderProperty" );
 
+	// Reset stored shader so it can reassess conditions
+	shader = "";
+
 	if ( iLSP.isValid() ) {
 		if ( !bslsp )
 			bslsp = properties.get<BSLightingShaderProperty>();

src/gl/glmesh.h
@@ -134,8 +134,8 @@ protected:
 	QVector<BoneWeights> weights;
 	QVector<SkinPartition> partitions;
 
-	//! Holds the name of the shader, or "fixed function pipeline" if no shader
-	QString shader;
+	//! Holds the name of the shader, or "" if no shader
+	QString shader = "";
 
 	//! Shader property
 	BSShaderLightingProperty * bssp = nullptr;

res/shaders/fo4_default.frag
@@ -1,4 +1,5 @@
 #version 130
+#extension GL_ARB_shader_texture_lod : require
 
 uniform sampler2D BaseMap;
 uniform sampler2D NormalMap;
@@ -6,6 +7,8 @@ uniform sampler2D GlowMap;
 uniform sampler2D BacklightMap;
 uniform sampler2D SpecularMap;
 uniform sampler2D GreyscaleMap;
+uniform sampler2D EnvironmentMap;
+uniform samplerCube CubeMap;
 
 uniform vec3 specColor;
 uniform float specStrength;
@@ -30,6 +33,8 @@ uniform bool hasSoftlight;
 uniform bool hasBacklight;
 uniform bool hasRimlight;
 uniform bool hasTintColor;
+uniform bool hasCubeMap;
+uniform bool hasEnvMask;
 uniform bool hasSpecularMap;
 uniform bool greyscaleColor;
 uniform bool doubleSided;
@@ -37,6 +42,8 @@ uniform bool doubleSided;
 uniform float subsurfaceRolloff;
 uniform float rimPower;
 uniform float backlightPower;
+
+uniform float envReflection;
 
 uniform mat4 worldMatrix;
 
@@ -229,6 +236,10 @@ void main( void )
 	float VdotH = max( dot(V, H), FLT_EPSILON );
 	float NdotNegL = max( dot(normal, -L), FLT_EPSILON );
 
+	vec3 reflected = reflect( V, normal );
+	vec3 reflectedVS = b * reflected.x + t * reflected.y + N * reflected.z;
+	vec3 reflectedWS = vec3( worldMatrix * (gl_ModelViewMatrixInverse * vec4( reflectedVS, 0.0 )) );
+
 	vec4 color;
 	vec3 albedo = baseMap.rgb * C.rgb;
 	vec3 diffuse = A.rgb + D.rgb * NdotL0;
@@ -262,6 +273,20 @@ void main( void )
 		specMask = s * specStrength;
 		
 		spec = TorranceSparrow( NdotL0, NdotH, NdotV, VdotH, vec3(specMask), fSpecularPower, 0.2 ) * NdotL0 * D.rgb * specColor;
+	}
+
+	// Environment
+	vec4 cube = textureLod( CubeMap, reflectedWS, 8.0 - smoothness * 8.0 );
+	vec4 env = texture2D( EnvironmentMap, offset );
+	if ( hasCubeMap ) {
+		cube.rgb *= envReflection * specStrength;
+		if ( hasEnvMask ) {
+			cube.rgb *= env.r;
+		} else {
+			cube.rgb *= s;
+		}
+		
+		spec += cube.rgb * diffuse;
 	}
 
 	vec3 backlight = vec3(0.0);

res/shaders/fo4_default.prog
@@ -1,19 +1,11 @@
 # default shader
 
-checkgroup begin or
-	# Fallout 4 and later
-	checkgroup begin and
-		check HEADER/Version >= 0x14020007
-		check HEADER/User Version >= 12
-        check HEADER/User Version 2 >= 130
-	checkgroup end
-checkgroup end
-
 checkgroup begin and
+	# Fallout 4
+    check HEADER/User Version 2 >= 130
     check BSLightingShaderProperty
-    check BSLightingShaderProperty/Skyrim Shader Type != 1
-    check BSLightingShaderProperty/Skyrim Shader Type != 16
-    #check NiAVObject/Vertex Size >= 3
+    #check BSLightingShaderProperty/Skyrim Shader Type != 1
+    #check BSLightingShaderProperty/Skyrim Shader Type != 16
     checkgroup begin or
         check BSTriShape
         check BSSubIndexTriShape
@@ -24,5 +16,7 @@ checkgroup end
 texcoords 0 base
 texcoords 1 tangents
 texcoords 2 bitangents
+texcoords 3 indices
+texcoords 4 weights
 
 shaders fo4_default.vert fo4_default.frag

res/shaders/fo4_default.vert
@@ -13,20 +13,40 @@ out vec4 C;
 out vec4 D;
 
 
+uniform bool isGPUSkinned;
+uniform mat4 boneTransforms[100];
+
 void main( void )
 {
-	gl_Position = ftransform();
+	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 	gl_TexCoord[0] = gl_MultiTexCoord0;
 	
-	N = normalize(gl_NormalMatrix * gl_Normal);
-	t = normalize(gl_NormalMatrix * gl_MultiTexCoord1.xyz);
-	b = normalize(gl_NormalMatrix * gl_MultiTexCoord2.xyz);
-	
+	if ( !isGPUSkinned ) {
+		N = normalize(gl_NormalMatrix * gl_Normal);
+		t = normalize(gl_NormalMatrix * gl_MultiTexCoord1.xyz);
+		b = normalize(gl_NormalMatrix * gl_MultiTexCoord2.xyz);
+		v = vec3(gl_ModelViewMatrix * gl_Vertex);
+	} else {
+		mat4 bt = boneTransforms[int(gl_MultiTexCoord3[0])] * gl_MultiTexCoord4[0];
+		bt += boneTransforms[int(gl_MultiTexCoord3[1])] * gl_MultiTexCoord4[1];
+		bt += boneTransforms[int(gl_MultiTexCoord3[2])] * gl_MultiTexCoord4[2];
+		bt += boneTransforms[int(gl_MultiTexCoord3[3])] * gl_MultiTexCoord4[3];
+
+		vec4 V = bt * gl_Vertex;
+		vec3 normal = vec3(bt * vec4(gl_Normal, 0.0));
+		vec3 tan = vec3(bt * vec4(gl_MultiTexCoord1.xyz, 0.0));
+		vec3 bit = vec3(bt * vec4(gl_MultiTexCoord2.xyz, 0.0));
+
+		gl_Position = gl_ModelViewProjectionMatrix * V;
+		N = normalize(gl_NormalMatrix * normal);
+		t = normalize(gl_NormalMatrix * tan);
+		b = normalize(gl_NormalMatrix * bit);
+		v = vec3(gl_ModelViewMatrix * V);
+	}
+
 	mat3 tbnMatrix = mat3(b.x, t.x, N.x,
                           b.y, t.y, N.y,
                           b.z, t.z, N.z);
-						  
-	v = vec3(gl_ModelViewMatrix * gl_Vertex);
 	
 	ViewDir = tbnMatrix * -v.xyz;
 	LightDir = tbnMatrix * gl_LightSource[0].position.xyz;

res/shaders/fo4_effectshader.frag
@@ -1,6 +1,6 @@
 #version 130
 
-uniform sampler2D SourceTexture;
+uniform sampler2D BaseMap;
 uniform sampler2D GreyscaleMap;
 uniform samplerCube CubeMap;
 uniform sampler2D NormalMap;
@@ -57,7 +57,7 @@ void main( void )
 {
 	vec2 offset = gl_TexCoord[0].st * uvScale + uvOffset;
 	
-	vec4 baseMap = texture2D( SourceTexture, offset );
+	vec4 baseMap = texture2D( BaseMap, offset );
 	vec4 normalMap = texture2D( NormalMap, offset );
 	vec4 specMap = texture2D( SpecularMap, offset );
 	
@@ -110,13 +110,13 @@ void main( void )
 	color.a = alphaMult * C.a * baseMap.a;
 	
 	if ( greyscaleColor ) {
-		vec4 luG = colorLookup( texture2D( SourceTexture, offset ).g, baseColor.r * C.r * falloff );
+		vec4 luG = colorLookup( texture2D( BaseMap, offset ).g, baseColor.r * C.r * falloff );
 
 		color.rgb = luG.rgb;
 	}
 	
 	if ( greyscaleAlpha ) {
-		vec4 luA = colorLookup( texture2D( SourceTexture, offset ).a, color.a );
+		vec4 luA = colorLookup( texture2D( BaseMap, offset ).a, color.a );
 		
 		color.a = luA.a;
 	}

res/shaders/fo4_effectshader.prog
@@ -1,23 +1,15 @@
 # effect shader
 
-checkgroup begin or
-	# Fallout 4 and later
-	checkgroup begin and
-		check HEADER/Version >= 0x14020007
-		check HEADER/User Version >= 12
-        check HEADER/User Version 2 >= 130
-	checkgroup end
-checkgroup end
-
-checkgroup begin or
+checkgroup begin and
 	# Fallout 4
-	checkgroup begin and
-		check BSEffectShaderProperty
-	checkgroup end
+    check HEADER/User Version 2 >= 130
+	check BSEffectShaderProperty
 checkgroup end
 
 texcoords 0 base
 texcoords 1 tangents
 texcoords 2 bitangents
+texcoords 3 indices
+texcoords 4 weights
 
 shaders fo4_effectshader.vert fo4_effectshader.frag

res/shaders/fo4_effectshader.vert
@@ -3,29 +3,49 @@
 out vec3 LightDir;
 out vec3 ViewDir;
 
-out vec4 A;
-out vec4 C;
-out vec4 D;
-
 out vec3 N;
 out vec3 t;
 out vec3 b;
 out vec3 v;
 
+out vec4 A;
+out vec4 C;
+out vec4 D;
+
+uniform bool isGPUSkinned;
+uniform mat4 boneTransforms[100];
+
 void main( void )
 {
-	gl_Position = ftransform();
+	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 	gl_TexCoord[0] = gl_MultiTexCoord0;
 	
-	N = normalize(gl_NormalMatrix * gl_Normal);
-	t = normalize(gl_NormalMatrix * gl_MultiTexCoord1.xyz);
-	b = normalize(gl_NormalMatrix * gl_MultiTexCoord2.xyz);
+	if ( !isGPUSkinned ) {
+		N = normalize(gl_NormalMatrix * gl_Normal);
+		t = normalize(gl_NormalMatrix * gl_MultiTexCoord1.xyz);
+		b = normalize(gl_NormalMatrix * gl_MultiTexCoord2.xyz);
+		v = vec3(gl_ModelViewMatrix * gl_Vertex);
+	} else {
+		mat4 bt = boneTransforms[int(gl_MultiTexCoord3[0])] * gl_MultiTexCoord4[0];
+		bt += boneTransforms[int(gl_MultiTexCoord3[1])] * gl_MultiTexCoord4[1];
+		bt += boneTransforms[int(gl_MultiTexCoord3[2])] * gl_MultiTexCoord4[2];
+		bt += boneTransforms[int(gl_MultiTexCoord3[3])] * gl_MultiTexCoord4[3];
+
+		vec4 V = bt * gl_Vertex;
+		vec3 normal = vec3(bt * vec4(gl_Normal, 0.0));
+		vec3 tan = vec3(bt * vec4(gl_MultiTexCoord1.xyz, 0.0));
+		vec3 bit = vec3(bt * vec4(gl_MultiTexCoord2.xyz, 0.0));
+
+		gl_Position = gl_ModelViewProjectionMatrix * V;
+		N = normalize(gl_NormalMatrix * normal);
+		t = normalize(gl_NormalMatrix * tan);
+		b = normalize(gl_NormalMatrix * bit);
+		v = vec3(gl_ModelViewMatrix * V);
+	}
 	
 	mat3 tbnMatrix = mat3(b.x, t.x, N.x,
                           b.y, t.y, N.y,
                           b.z, t.z, N.z);
-						  
-	v = vec3(gl_ModelViewMatrix * gl_Vertex);
 	
 	ViewDir = tbnMatrix * -v.xyz;
 	LightDir = tbnMatrix * gl_LightSource[0].position.xyz;

commit 6798b9a1bfe72f993625a740efc839b40c7f3d6a

[GL] Reset shader when shading actions are toggled
Fix issue caused by refactor in 31fc1b2 where you could not turn shaders back on after turning them off.

src/gl/bsshape.cpp
@@ -34,7 +34,7 @@ void BSShape::clear()
 
 void BSShape::update( const NifModel * nif, const QModelIndex & index )
 {
-	Node::update( nif, index );
+	Shape::update( nif, index );
 
 	if ( !iBlock.isValid() || !index.isValid() )
 		return;

src/gl/glmesh.cpp
@@ -107,6 +107,19 @@ void Shape::setController( const NifModel * nif, const QModelIndex & iController
 	}
 }
 
+
+void Shape::update( const NifModel * nif, const QModelIndex & index )
+{
+	Node::update( nif, index );
+
+	// If shaders are reenabled, reset
+	if ( !(scene->options & Scene::DisableShaders) && shader.isNull() 
+		 && nif->checkVersion( 0x14020007, 0x14020007 ) )
+	{
+		updateShaderProperties( nif );
+	}
+}
+
 void Shape::updateShaderProperties( const NifModel * nif )
 {
 	auto prop = nif->getLink( iBlock, "Shader Property" );
@@ -182,7 +195,7 @@ void Shape::boneSphere( const NifModel * nif, const QModelIndex & index ) const
 
 void Mesh::update( const NifModel * nif, const QModelIndex & index )
 {
-	Node::update( nif, index );
+	Shape::update( nif, index );
 
 	// Was Skinning toggled?
 	// If so, switch between partition triangles and data triangles

src/gl/glmesh.h
@@ -60,6 +60,8 @@ public:
 	Shape( Scene * s, const QModelIndex & b );
 	~Shape() { clear(); }
 
+	void update( const NifModel * nif, const QModelIndex & ) override;
+
 	virtual void drawVerts() const {};
 	virtual QModelIndex vertexAt( int ) const { return QModelIndex(); };
 

src/nifskope_ui.cpp
@@ -250,6 +250,7 @@ void NifSkope::initActions()
 
 	shadingActions = agroup( { ui->aTextures, ui->aVertexColors, ui->aSpecular, ui->aGlow, ui->aCubeMapping, ui->aLighting, ui->aDisableShading }, false );
 	connect( shadingActions, &QActionGroup::triggered, ogl->getScene(), &Scene::updateSceneOptionsGroup );
+	connect( shadingActions, &QActionGroup::triggered, ogl, &GLView::updateScene );
 
 	auto testActions = agroup( { ui->aTest1Dbg, ui->aTest2Dbg, ui->aTest3Dbg }, true );
 	connect( testActions, &QActionGroup::triggered, ogl->getScene(), &Scene::updateSceneOptionsGroup );